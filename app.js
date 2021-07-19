487e4b47be7949814b96129394a69848e37ee9a7
VIncentTetteh <vincentchrisbone@gmail.com>
2021-07-19 16:20:46 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My first AWS DevOps tutorials!'));
+app.get('/', (req, res) => res.send('My first AWS DevOps tutorials!.' +
+    'I have been able to create a pipe from source to build, review and deploy'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
